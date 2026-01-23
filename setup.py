e75936dfc42f15a40d1b8c3eb6dcf3ccbd3b7f69
shaaiir <139155163+shaaiir@users.noreply.github.com>
2026-01-23 20:49:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-logger-utils",
-    version="1.0.0",
+    version="1.0.1",
     description="Helper utilities for logging and debugging",
     author="Open Source Community",
     packages=find_packages(),
